79ea2fc4cd9921d0c3ebfe1b6921621e81aca67e
milad25haidari <milad25haidari@users.noreply.github.com>
2022-07-12 21:09:37 -0700

    jvscrpt

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,11 +212,11 @@
    */
   new Swiper('.portfolio-details-slider', {
     speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
+    loop: false,
+    // autoplay: {
+    //   delay: 8000,
+    //   disableOnInteraction: false
+    // },
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
